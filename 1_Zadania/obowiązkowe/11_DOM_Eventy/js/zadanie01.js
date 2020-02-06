f643a7bb17d75fb5b7d566a5ce3b3f3b51c2b385
Filip Kosmala <kosmala.filip@gmail.com>
2020-02-06 20:43:57 +0100

    Add remaining solutions

--- 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie01.js
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie01.js
@@ -1,5 +1,25 @@
-var menu = document.querySelector("#menu");
-var paragraf = document.querySelector("p");
+document.addEventListener("DOMContentLoaded", function () {
 
-menu.classList.add("menu");
-paragraf.innerHTML = "A to jest paragraf w zadaniu 1";
+    var menu = document.querySelector("#menu");
+    var paragraf = document.querySelector("p");
+
+    menu.classList.add("menu");
+    paragraf.innerHTML = "A to jest paragraf w zadaniu 1";
+
+    //Zadanie 1
+
+    //1.1
+    // w pliku skryptu próbujemy uzyskać element, gdy nie istnieje on na stronie
+    //pierw ładuje się kod javascript, a pozniej HTML
+
+
+    //1.2
+    // event "DOMContentLoaded" "mowi" przegladarce ze musi poczekac 
+    //zanim strona zaladuje strona a dopiero pozniej odpalic skrypt
+
+    //1.3 
+    //Przeniesieni pliku na dol stromny nic nie zmieni 
+    //nasz event "DOMContentLoaded" czeka na strone az sie zaladuje
+
+    
+});
---
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie06.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    
+   let box = document.getElementById("box");
+   box.addEventListener('mousemove', onMouseMoveGlobal)
+    box.addEventListener('mousemove', onMouseMoveLocal)
+    let getGlobalX = document.getElementById("globalX");
+    let getGlobalY = document.getElementById("globalY");
+    let getLocalX = document.getElementById("localX");
+    let getLocalY = document.getElementById("localY");
+    function onMouseMoveGlobal(e){
+       getGlobalX.textContent = e.x;
+       getGlobalY.textContent = e.y;
+    }
+    function onMouseMoveLocal(e){
+        let rect = box.getBoundingClientRect(); 
+        let x = e.clientX - rect.left; 
+        let y = e.clientY - rect.top; 
+        getLocalY.textContent = x;
+        getLocalX.textContent = y;
+     }
+    
+});
